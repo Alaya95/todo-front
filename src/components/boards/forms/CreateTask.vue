542c4e29550cb10188a48224c0576d52d0cac9f4
Alaya95 <rozetta.95@mail.ru>
2021-07-08 08:25:49 +0300

    Добавлено модальное окно создания задачи для колонки

---
+++ src/components/boards/forms/CreateTask.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="create-task">
+    <div class="create-task_blur" @click="closeCreateTaskForm">
+    </div>
+    <div class="create-task_form">
+      <p>Создать новую задачу</p>
+      <input type="text">
+      <textarea id="descriptionTask" cols="30" name="description" rows="8"></textarea>
+      <button @click="closeCreateTaskForm">
+        Добавить задачу
+      </button>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CreateTask",
+  props: ['closeCreateTaskForm']
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "src/layout/scss/forms/boards/createTask.scss";
+</style>
