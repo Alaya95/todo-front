1967dcf6e7fd08f7c86e1b095d5fa23c974cd7de
Alaya95 <rozetta.95@mail.ru>
2021-07-08 18:45:36 +0300

    Добавлена id, name для input, textarea vue компонента createTask

--- src/components/boards/forms/CreateTask.vue
+++ src/components/boards/forms/CreateTask.vue
@@ -4,22 +4,19 @@
     </div>
     <div class="create-task_form">
       <p>Создать новую задачу</p>
-      <input type="text">
-      <textarea id="descriptionTask" cols="30" name="description" rows="8"></textarea>
+      <input name="taskName" type="text">
+      <textarea id="descriptionTask" cols="30" name="descriptionTask" rows="8"></textarea>
       <button @click="closeCreateTaskForm">
         Добавить задачу
       </button>
     </div>
-
-
   </div>
 </template>
 
 <script>
 export default {
   name: "CreateTask",
-  props: ['closeCreateTaskForm']
-
+  props: ['closeCreateTaskForm'],
 }
 </script>
 
