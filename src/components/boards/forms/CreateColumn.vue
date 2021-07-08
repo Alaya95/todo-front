4f65b25f663ab85f70fdaf15165e8a04b7c101ce
Alaya95 <rozetta.95@mail.ru>
2021-07-08 18:46:06 +0300

    Добавлен vue  компонента createColumn

---
+++ src/components/boards/forms/CreateColumn.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="create-task">
+    <div class="create-task_blur" @click="closeCreateColumnForm">
+    </div>
+    <div class="create-task_form">
+      <p>Создать новую колонку</p>
+      <p>введите название колонки</p>
+
+      <input name="columnName" id="columnName" type="text">
+
+      <button @click="fetchDesks">
+        Добавить задачу
+      </button>
+
+    </div>
+  </div>
+</template>
+
+<script>
+//import store from "../../../store/store";
+
+export default {
+  name: "CreateColumn",
+  props: ['closeCreateColumnForm'],
+  methods: {
+   /* fetchDesks() {
+      //return
+      console.log(this.$store.dispatch(''))
+    }*/
+
+
+   /* enter() {
+      const data = {
+        name: document.getElementById("columnName").value,
+      };
+      store.dispatch('createColumn', data);
+    }*/
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "src/layout/scss/forms/boards/createTask.scss";
+</style>
