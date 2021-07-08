738cd22894c8d8d66684effb0d07d0cc8ac16831
Alaya95 <rozetta.95@mail.ru>
2021-07-08 09:19:44 +0300

    исправлена ошибка авторизация

--- src/modules/api.js
+++ src/modules/api.js
@@ -5,7 +5,7 @@ export default async function(url, method = "GET", data) {
     method,
     headers: {
       "Content-type": "application/json",
-      "X-XSRF-TOKEN": localStorage.getItem("token"),
+      "Authorization": "Bearer " + localStorage.getItem("token"),
     },
     body: JSON.stringify(data),
   }).then((res) => res.json());
