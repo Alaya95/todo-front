6ef2985d74817008bce38b2a96f10f30a41a81c8
Alaya95 <rozetta.95@mail.ru>
2021-07-15 01:04:31 +0300

    Board. CreateTask.vue. Добавлено закрытие формы при нажатии на кнопку "Добавить".

--- src/components/boards/forms/CreateTask.vue
+++ src/components/boards/forms/CreateTask.vue
@@ -5,7 +5,7 @@
     <div class="create-task_form">
       <p>Создать новую задачу</p>
       <label :for='("nameTask" + id )'>Название задачи</label>
-      <input  :id='("nameTask" + id )' ref="nameTask" name="nameTask" type="text">
+      <input :id='("nameTask" + id )' ref="nameTask" name="nameTask" type="text">
 
       <label :for='("descriptionTask" + id )'>Описание задачи</label>
       <textarea :id='("descriptionTask" + id )' ref="descriptionTask" cols="30" name="descriptionTask" rows="8"></textarea>
@@ -39,7 +39,7 @@ export default {
         name: 2, //this.$refs.nameTask.value,
         description: 2, //this.$refs.descriptionTask.value,
         deadline: '2021-07-10 21:16', //this.$refs.deadlineTask.value,
-        period:  '2021-07-10 21:16',//this.$refs.periodTask.value,
+        period: '2021-07-10 21:16',//this.$refs.periodTask.value,
         board_id: 1,
         executor_id: 1,
         initiator_id: 1,
@@ -47,14 +47,17 @@ export default {
         task_status: 1
       };
       store.dispatch('createTask', data)
-    // const index = Object.values(data).findIndex(elem => !elem);
-    // index === -1 && store.dispatch('createTask', data)
 
-    // Object.values(data).forEach( elem => {
-    //   console.log(elem)
-    //   elem = ''
+      /* при нажатии на кнопку добавить, отправляется запрос и закрывается модалка */
+      this.closeCreateTaskForm()
+      // const index = Object.values(data).findIndex(elem => !elem);
+      // index === -1 && store.dispatch('createTask', data)
 
-    // } )
+      // Object.values(data).forEach( elem => {
+      //   console.log(elem)
+      //   elem = ''
+
+      // } )
     }
   },
 }
