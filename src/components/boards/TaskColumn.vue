1dce1c1e3d88d88fa0057a2e0cb78c6269f78c77
Alaya95 <rozetta.95@mail.ru>
2021-07-15 01:11:20 +0300

    Board. TaskColumn.vue. исправлено открытие и закрытие меню. При нажатии кнопки изменить или удалить данные отправляются на сервер и форма закрывается. Поправлен баг с возвращением кнопки редактировать вместо кнопки изменить после закрытия меню. реализованно удаление, редактирования колонки. board_id является временно статическим.

--- src/components/boards/TaskColumn.vue
+++ src/components/boards/TaskColumn.vue
@@ -6,18 +6,19 @@
     <div :draggable="false" class="board-column_title" >
 
       <p  v-if='editTitleColumn' :draggable="false">{{ column.title }}</p>
-      <input v-else type="text" name="columnName" :value="column.title">
+      <input v-else type="text" ref="columnName" name="columnName" :value="column.title">
 
       <!-- кнопка для выпадающего меню для последующего редактирования названия колонки -->
-      <button @click="showColumnMenu = !showColumnMenu" :draggable="false" aria-current="page">
+      <button @click="openMenu" :draggable="false" aria-current="page">
         <i class="fas fa-ellipsis-h"></i>
       </button>
     </div>
 
     <!-- выпадающее меню колонки -->
-    <div v-show="showColumnMenu" class="board-column_menu">
-      <button @click="editTitleColumn = !editTitleColumn ">Редактировать</button>
-      <button>Удалить</button>
+    <div v-show="isOpenMenu" class="board-column_menu">
+      <button v-if="editTitleColumn" @click="editTitleColumn = !editTitleColumn ">Редактировать</button>
+      <button v-else @click="editColumn">Изменить</button>
+      <button @click="deleteColumn">Удалить</button>
     </div>
 
     <!-- рендер блоков с задачами -->
@@ -41,8 +42,6 @@
       <CreateTask :id="column.id"  v-show="openCreateTask" :closeCreateTaskForm="closeCreateTaskForm" />
 
     </div>
-
-
   </div>
 </template>
 
@@ -51,7 +50,6 @@ import TaskInColumn from "./TaskInColumn";
 import CreateTask from "./forms/CreateTask";
 export default {
   name: "TaskColumn",
-
   components: {
     CreateTask,
     TaskInColumn
@@ -60,15 +58,24 @@ export default {
   data: () => {
     return {
       order: 1,
-      showColumnMenu: false,
-      editTitleColumn: true,
       createTask: true,
       openCreateTask: false,
+
+      /*j открытие и закрытие меню колонки */
+      isOpenMenu: false,
+      editTitleColumn: true,
     }
   },
   methods: {
+    openMenu() {
+      this.isOpenMenu = !this.isOpenMenu;
+    },
+    closeMenu() {
+      this.isOpenMenu = false;
+      this.editTitleColumn = true;
+    },
     openCreateTaskForm() {
-      this.openCreateTask = true;
+      this.openCreateTask = !this.openCreateTask;
     },
     closeCreateTaskForm() {
       this.openCreateTask = false;
@@ -78,6 +85,25 @@ export default {
     },
     drop(e) {
       this.$store.dispatch("moveTask", {e, id: this.column.id, order: this.order});
+    },
+    editColumn() {
+      const data = {
+        board_id: 1,
+        id: this.column.id,
+        title: this.$refs.columnName.value
+      };
+
+      this.$store.dispatch('editColumn', data);
+
+      this.closeMenu()
+    },
+    deleteColumn() {
+      const data = {
+        board_id: 1,
+        id: this.column.id
+      }
+      this.$store.dispatch('deleteColumn', data)
+      this.closeMenu()
     }
   },
 };
