d630d1877038eeca02958210b3cadc61a46e6809
Alaya95 <rozetta.95@mail.ru>
2021-07-14 14:05:28 +0300

    исправление доски

--- src/components/boards/TaskColumn.vue
+++ src/components/boards/TaskColumn.vue
@@ -1,14 +1,14 @@
 <template>
 
   <div class="board-column" :id="column.id" @dragover.prevent @drop.prevent="drop" @dragenter.prevent>
-    <div class="board-column_title" >
+    <div :draggable="false" class="board-column_title" >
       <p :draggable="false">{{ column.title }}</p>
       <button :draggable="false" aria-current="page">
         <i class="fas fa-ellipsis-h"></i>
       </button>
     </div>
 
-    <div  class="column-width">
+    <div  class="column-width" >
       <TaskInColumn
           v-for="task in column.tasks"
           :key="task.id"
@@ -17,7 +17,7 @@
           draggable="true"
       />
 
-      <div class="board-column_addTask">
+      <div :draggable="false"  class="board-column_addTask">
         <a :draggable="false" class="#">
           <i  :draggable="false" class="fas fa-plus-square"></i>
           Добавить задачу
