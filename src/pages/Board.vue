abb4e1926b7cc7e5004f5718cb87728763539193
Alaya95 <rozetta.95@mail.ru>
2021-07-14 22:41:33 +0300

    Board. board. добавлена кнопка для создания колонки.

--- src/pages/Board.vue
+++ src/pages/Board.vue
@@ -2,7 +2,16 @@
   <div class="account-content">
     <NavbarAccount />
     <div class="board">
+
       <TaskColumn v-for="column in getDesk" :key="column.id" :column="column" />
+
+      <div class="board-column_addTask">
+        <button @click="openCreateColumnForm">
+          <i class="fas fa-plus-square"></i>
+          Добавить колонку
+        </button>
+      </div>
+
     </div>
   </div>
 </template>
@@ -12,17 +21,26 @@ import NavbarAccount from "../components/account/NavbarAccount";
 import TaskColumn from "../components/boards/TaskColumn";
 import {mapGetters} from "vuex";
 
-
 export default {
   name: "Board",
   components: {
-    //CreateColumn,
     NavbarAccount,
     TaskColumn,
+  },
+  data() {
+    return {
+      openCreateColumn: false,
+    };
   },
   methods: {
     fetchDesk() {
       this.$store.dispatch('fetchDesk')
+    },
+    openCreateColumnForm() {
+      this.openCreateColumn = true;
+    },
+    closeCreateColumnForm() {
+      this.openCreateColumn = false;
     },
   },
   mounted() {
