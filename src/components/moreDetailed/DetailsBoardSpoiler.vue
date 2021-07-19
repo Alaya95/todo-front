00b88e133d42f48f8806c7e4baa5f474c5480384
Alaya95 <rozetta.95@mail.ru>
2021-07-19 16:14:25 +0300

    страница всех досок пользователя. добавлен input для изменения названия доски. добавлены кнопки для редактирования и удаления доски. добавлена ссылка для перехода на рабочее пространство доски. подключен boardModule для изменения названия и удаления доски.

--- src/components/moreDetailed/DetailsBoardSpoiler.vue
+++ src/components/moreDetailed/DetailsBoardSpoiler.vue
@@ -1,108 +1,110 @@
 <template>
+  <div class="board accounts">
+    <div :id="board.id" ref="boardId" class="board-name">
 
-  <div>
-    <div class="tusk accounts" >
-      <details>
-        <summary>
-          <label class="group-name">
-            Название доски
-          </label>
+      <router-link v-if="isOpenEditTitle" :to='"board/" + board.id'> {{ board.name }}</router-link>
+      <input v-else ref="titleBoard" :value="board.name" name="titleBoard" type="text">
 
-          <div class="icons">
-            <a href="#"><i class="fas fa-edit"></i></a>
-
-            <a href="#">
-              <i class="fas fa-trash-alt">
-              </i></a>
-          </div>
-
-        </summary>
-
-        <div class="group-description">
-          <div class="group-users">
-            <label>Задачи доски</label>
-            <p>Задача 1</p>
-            <p>Задача 2</p>
-            <p>Задача 3</p>
-          </div>
-          <div class="group-desk">
-            <label>Срок задачи</label>
-            <p>20.07.2021</p>
-            <p>20.07.2021</p>
-            <p>20.07.2021</p>
-          </div>
-        </div>
-      </details>
     </div>
+    <div class="board-icons">
+
+      <button @click="editTitleBoard"><i class="fas fa-edit"></i></button>
+      <button @click="deleteBoard"><i class="fas fa-trash-alt"></i></button>
+
+    </div>
+
   </div>
 </template>
-
-
 <script>
 
+export default {
+  name: 'DetailsBoardSpoiler',
+  props: ['board'],
+  data() {
+    return {
+      isOpenEditTitle: true
+    }
+  },
+  methods: {
+    editTitleBoard() {
+      const data = {
+        id: this.$refs.boardId.id,
+        name: this.$refs.titleBoard.value
+      }
+      this.$store.dispatch('editBoards', data)
+    },
+    deleteBoard() {
+      const data = {
+        id: this.$refs.boardId.id,
+      }
+
+      this.$store.dispatch('deleteBoard', data)
+    }
+  }
+
+}
 
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
-.icons {
-  margin-right: 15px;
-  margin-top: 15px;
-  display: flex;
-  width: 7%;
-  justify-content: space-around;
-}
-.tusk {
-
-  width: 91%;
-  min-height: 60px;
-  /*width: initial;*/
+.board {
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
   align-items: center;
-  border: 2px solid #32B9A1;
-  margin: 0 10px 20px 5px;
-  border-radius: 8px;
-}
-.accounts {
-  /*height: 100%;*/
-  overflow: hidden;
-  width: initial;
-  display: flex;
-  flex-direction: row;
   justify-content: space-between;
-  border-radius: 8px;
-  background: #414141;
+  margin-bottom: 10px;
+  background-color: #2c3e50;
+  border-radius: 10px;
 
+  &-name {
+    margin: 10px 10px 10px 20px;
+
+    & a {
+      text-decoration: none;
+      font-weight: bolder;
+      color: #ffffff;
+
+      &:hover {
+        color: #38c172;
+      }
+    }
+  }
+
+  &-icons {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin: 10px 10px 10px 20px;
+
+    & button {
+      background: none;
+      border: none;
+      outline: none;
+      margin: 7px;
+
+      &:first-child {
+        &:hover {
+          color: #32B9A1;
+
+          & i {
+            color: #32B9A1;
+          }
+        }
+      }
+
+      &:last-child {
+        &:hover {
+          color: #e3342f;
+
+          & i {
+            color: #e3342f;
+          }
+        }
+      }
+    }
+  }
 }
-summary {
-  display: flex;
-  background: #414141;
-  justify-content: space-between;
-  min-height: 55px;
-  margin-bottom: 1px;
-  margin-left: 10px;
 
-}
-
-details {
-  flex: 1 1 50px;
-  background: #414141;
-  text-align: justify;
-
-}
-
-.group-name {
-  margin-top: 15px;
-  pointer-events: none;
-}
-
-.group-description{
-  display: flex;
-  justify-content: space-between;
-  pointer-events: none;
-  padding-left: 20px;
-  padding-right: 20px;
-}
 </style>
