82d4df8aacf84e5b35133e4b58d99161c79dd0ad
Alaya95 <rozetta.95@mail.ru>
2021-07-19 23:32:35 +0300

    исправлен props, удалены лишние отступы и переносы строки.

--- src/components/tasks/TaskComments.vue
+++ src/components/tasks/TaskComments.vue
@@ -1,110 +1,86 @@
 <template>
-
-    <div class="comment">
-        <div class="commentHeader">
-            <img src="../../assets/avatar.png" alt="avatar">
-            <a href="#">{{user.name}}</a>
-            <p>{{comment.updated_at | date}}</p>
-        </div>
-
-        <div class="commentText" :id="comment.id" ref="commentId">
-            <p v-if="editComment">{{comment.content}}</p>
-            <textarea
-                    v-else :id='("nameTask" + comment.content)'
-                    ref="editComment" name="editComment"
-                    :value="comment.content" type="text"
-
-            ></textarea>
-        </div>
-
-        <div class="commentEdit">
-            <button type="button" v-if="!isHiddenChange" @click="openInput">Редактировать</button>
-            <button type="button" v-if="isHiddenSave" @click="changeTaskFormComments(comment, index)">Сохранить</button>
-            <button type="button" @click="deleteTaskFormComments(comment)">Удалить</button>
-        </div>
+  <div class="comment">
+    <div class="commentHeader">
+      <img alt="avatar" src="../../assets/avatar.png">
+      <a>{{ user.name }}</a>
+      <p>{{ comment.updated_at | date }}</p>
     </div>
 
+    <div :id="comment.id" ref="commentId" class="commentText">
+      <p v-if="editComment">{{ comment.content }}</p>
+      <textarea
+          v-else :id='("nameTask" + comment.content)'
+          ref="editComment" :value="comment.content"
+          name="editComment" type="text"
+      ></textarea>
+    </div>
+
+    <div class="commentEdit">
+      <button v-if="!isHiddenChange" type="button" @click="openInput">Редактировать</button>
+      <button v-if="isHiddenSave" type="button" @click="changeTaskFormComments(comment, index)">Сохранить</button>
+      <button type="button" @click="deleteTaskFormComments(comment)">Удалить</button>
+    </div>
+  </div>
 </template>
 
 <script>
-    import {mapGetters} from "vuex";
-    import store from "../../store/store";
-    import moment from 'moment';
+import {mapGetters} from "vuex";
+import store from "../../store/store";
+import moment from 'moment';
 
-    export default {
-        name: "TaskComments",
-        data() {
-            return {
-                editComment: true,
-                isHiddenChange: false,
-                isHiddenSave: false,
+export default {
+  name: "TaskComments",
+  data() {
+    return {
+      editComment: true,
+      isHiddenChange: false,
+      isHiddenSave: false,
+    };
+  },
+  props: ['index', 'user', 'comment'],
+  methods: {
+    changeTaskFormComments(comment, index) {
+      const data = {
+        id: comment.id,
+        content: this.$refs.editComment.value,
+        index: index
+      }
 
-            };
-        },
-        props: {
-            comment: {
-                type: Object,
-                required: true,
-            },
-            user: {
-                type: Object,
-                required: true,
-            },
-            key:{
-                type: Array,
-                required: true,
-            },
-        },
-        methods: {
-            changeTaskFormComments(comment, index) {
-                const data = {
-                    id: comment.id,
-                    content: this.$refs.editComment.value,
-                    index:index
-                }
-                console.log(data)
-                store.dispatch('editTaskFormComment', data)
-                this.closeInput();
+      store.dispatch('editTaskFormComment', data)
+      this.closeInput();
+    },
+    deleteTaskFormComments(comment) {
+      const data = {
+        id: comment.id
+      }
 
-            },
-            deleteTaskFormComments(comment) {
-                const data = {
-                    id: comment.id
-                }
+      store.dispatch('deleteTaskFormComment', data)
+    },
+    openInput() {
+      this.isHiddenChange = true;
+      this.editComment = false;
+      this.isHiddenSave = true;
+    },
 
-                store.dispatch('deleteTaskFormComment', data)
-            },
-            openInput() {
-                this.isHiddenChange = true;
-                this.editComment = false;
-                this.isHiddenSave = true;
-            },
-
-            closeInput() {
-                this.isHiddenChange = false;
-                this.editComment = true;
-                this.isHiddenSave = false;
-            },
-        },
-        computed: {
-            ...mapGetters([
-                'getTaskFormComments',
-
-            ]),
-        },
-
-        filters: {
-            date: function (value) {
-                if (value) {
-                    return moment(String(value)).format('MM/DD/YYYY hh:mm')
-                }
-            }
-        }
-
-
+    closeInput() {
+      this.isHiddenChange = false;
+      this.editComment = true;
+      this.isHiddenSave = false;
+    },
+  },
+  computed: {
+    ...mapGetters([
+      'getTaskFormComments',
+    ]),
+  },
+  filters: {
+    date: function (value) {
+      if (value) {
+        return moment(String(value)).format('MM/DD/YYYY hh:mm')
+      }
     }
+  }
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
