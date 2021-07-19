ea9d3b28043342331041176fac65fc6c1b390e6f
Alaya95 <rozetta.95@mail.ru>
2021-07-19 23:35:14 +0300

    добавлено удаление и редактирование комментариев для формы задачи

--- src/store/taskFormModule.js
+++ src/store/taskFormModule.js
@@ -7,28 +7,27 @@ const taskFormModule = {
     }),
     mutations: {
         fetchTaskForm(state, data) {
-
             const comments = data.result.filter(function (comment) {
                 return comment.task_id === parseInt(data.task_id);
             });
             state.task = {...state.task, comment: comments};
             state.isLoaded = true
-
         },
-
         getTask(state, task) {
             state.task = {...state.task, task: task}
             state.isLoaded = true
         },
         createComment(state, data) {
             state.task = {...state.task, comment: [...state.task.comment, data]};
-            console.log(data)
         },
-
-
+        editComment(state, data) {
+            state.task.comment.splice(state.task.comment.findIndex((item) => item.id == data.id), 1, data)
+        },
+        deleteComment(state, data) {
+            state.task.comment.splice(state.task.comment.findIndex((item) => item.id == data.id), 1)
+        }
     },
     actions: {
-// получить все комментарии
         async fetchTaskFormComments({commit}, task_id) {
             try {
                 const result = await api('comments')
@@ -37,61 +36,50 @@ const taskFormModule = {
                     const data = {
                         task_id, result
                     }
-
                     commit('fetchTaskForm', data)
                 }
             } catch (error) {
                 console.log(error)
             }
         },
-//получить задачу по ID
         async fetchTask({commit}, data) {
             try {
                 const result = await api('task/' + data);
-                commit('getTask', result)
 
+                if (result) {
+                    commit('getTask', result)
+                }
             } catch (error) {
                 console.log(error);
             }
         },
-
-        // записать комментарий
         async createTaskFormComment({commit}, data) {
             try {
                 const result = await api('comment/store', "POST", data);
 
                 if (!result.message) {
                     commit("createComment", result);
-                    console.log(result)
                 }
-
             } catch (error) {
                 console.log(error)
             }
-
         },
-        // редактировать комментарий
         async editTaskFormComment({commit}, data) {
             try {
-
                 const result = await api('comment/' + data.id, "put", data)
                 if (!result.message) {
-                    commit("createComment", result);
-
+                    commit("editComment", result);
                 }
             } catch (error) {
                 console.log(error)
             }
         },
-        //удалить комментарий
         async deleteTaskFormComment({commit}, data) {
-
             try {
-                // в юрл нужно передавать id самой задачи.
                 const result = await api('comment/' + data.id, "delete", data)
-                console.log(result, commit)
-                if (result) {
-                    console.log(result)
+
+                if (result.message) {
+                    commit('deleteComment', data)
                 }
             } catch (error) {
                 console.log(error)
@@ -105,10 +93,7 @@ const taskFormModule = {
         getTask(state) {
             return state.task
         },
-
-
     }
-
 }
 
 export default taskFormModule;
