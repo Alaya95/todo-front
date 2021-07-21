1ce1ae5229efc4fc2c62fac325bf484e8db3b84c
Alaya95 <rozetta.95@mail.ru>
2021-07-21 22:28:52 +0300

    изменен вывод данных

--- src/components/moreDetailed/DetailsTaskSpoiler.vue
+++ src/components/moreDetailed/DetailsTaskSpoiler.vue
@@ -1,19 +1,21 @@
 <template>
   <details class="spoiler">
     <summary>
-      <div class="summary-title">{{ task.task_name }}</div>
+      <div class="summary-title">{{ task.name }}</div>
       <div class="summary-status">{{ task.task_status }}</div>
     </summary>
 
     <div class="spoiler-info">
 
       <div class="spoiler-info_description">
-        {{ task.task_description }}
+        {{ task.description }}
       </div>
 
       <div class="spoiler-info_data">
-        <div>Дата создания: {{ task.created_at }}</div>
-        <div> срок сдачи: {{ task.task_period }}</div>
+        <div>Дата создания: {{ new Date(task.created_at).toLocaleDateString() }}</div>
+        <div> срок сдачи: {{ task.deadline === null && 'бессрочно' }}
+
+        </div>
       </div>
 
     </div>
