baff6c7f97e2548b192196fa2f06a35679044d2d
Alaya95 <rozetta.95@mail.ru>
2021-07-27 00:35:57 +0300

    Удалены лишние переносы строки

--- src/pages/Account.vue
+++ src/pages/Account.vue
@@ -1,17 +1,13 @@
 <template>
   <div class="content">
-
-    <Navbar />
-
-    <IndexAccount v-show="true" />
-
+    <Navbar/>
+    <IndexAccount v-show="true"/>
   </div>
 </template>
 
 <script>
 import Navbar from "../components/Navbar";
 import IndexAccount from "../components/account/IndexAccount";
-
 
 export default {
   name: "Account",
@@ -23,5 +19,5 @@ export default {
 </script>
 
 <style lang="scss">
-  @import "../layout/scss/account.scss";
+@import "../layout/scss/account.scss";
 </style>
