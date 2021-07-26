42a441b523c2f9edb67db5eb2b48bbc03f12d030
Alaya95 <rozetta.95@mail.ru>
2021-07-27 00:47:34 +0300

    Удалены лишние классы стилей, отступы и переносы строк

--- src/components/account/indexPage/AccountGroupBlock.vue
+++ src/components/account/indexPage/AccountGroupBlock.vue
@@ -1,68 +1,58 @@
 <template>
-  <div class="card col-xs-6 col-xl-7 m-sm-2 m-xl-3 account_groups">
-
-    <div class="bigBlock">
-
-      <div class="title">
-        <p>Ваши группы</p>
-       <!-- <router-link :to='{name: "usergroups"}'><i class="fas fa-ellipsis-h"></i></router-link>-->
-        <i class="fas fa-ellipsis-h"></i>
-      </div>
-
-      <div class="tusks" >
-        <AccountGroup
-            v-for="group in getGroups.slice(0,4)"
-            :key="group.item"
-            group_prop.sync="group"
-            v-bind:group="group"
-        />
-
-      </div>
-      <div class="tusks">
-        <button><i class="far fa-plus-square" v-on:click="show = !show;"> Cоздать группу</i>
-          </button>
-          <popupGroup
-              v-if="show"
-              @closePopup="ClosePopup"
-          />
-      </div>
+  <div class="bigBlock card account_groups">
+    <div class="title">
+      <p>Ваши группы</p>
+      <!-- <router-link :to='{name: "usergroups"}'><i class="fas fa-ellipsis-h"></i></router-link>-->
+      <i class="fas fa-ellipsis-h"></i>
+    </div>
+    <div class="tusks">
+      <AccountGroup
+          v-for="group in getGroups.slice(0,4)"
+          :key="group.item"
+          group_prop.sync="group"
+          v-bind:group="group"
+      />
+    </div>
+    <div class="tusks">
+      <button><i class="far fa-plus-square" v-on:click="show = !show;"> Cоздать группу</i>
+      </button>
+      <popupGroup
+          v-if="show"
+          @closePopup="ClosePopup"
+      />
     </div>
   </div>
-
 </template>
 <script>
 import AccountGroup from "./AccountGroup";
 import popupGroup from "./popupGroup";
 import {mapGetters} from "vuex";
-import store from "@/store/store";
 
 export default {
   name: "AccountGroupBlock",
-  components: {AccountGroup,popupGroup},
+  components: {
+    AccountGroup,
+    popupGroup
+  },
   data() {
     return {
       show: false,
-
     }
   },
-
   methods: {
     ClosePopup() {
       this.show = false;
     },
-
     removeGroup(id) {
-      const data =  {
+      const data = {
         id: id
       }
-      store.dispatch('deleteGroup', data)
+      this.$store.dispatch('deleteGroup', data)
     },
-
     fetchGroups() {
       this.$store.dispatch('fetchGroups')
     },
   },
-
   mounted() {
     this.fetchGroups();
   },
@@ -71,30 +61,6 @@ export default {
       'getGroups',
     ]),
   },
-
 }
 </script>
-<style scoped>
-
-
-.fa-plus-square:hover{
-  color: #3f8778;
-}
-button {
-  background: transparent;
-  border: none;
-  outline: none;
-  margin: 0.7rem;
-
-}
-
-.card{
-  width: 96%;
-  height: 100%;
-}
-.bigBlock{
-
-  height: 100%;
-}
-
-</style>
+<style scoped></style>
